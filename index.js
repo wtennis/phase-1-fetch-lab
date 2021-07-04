ccdbe29fccb85f1a6af4219270d5a583755396d7
Whiting <whitinghtennis@gmail.com>
2021-07-04 13:18:14 -0700

    commit tree cab40c387643eeaee40c0a8ffd861c026637ed72

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -11,6 +13,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
